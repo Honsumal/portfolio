51cbdef2318f7be77a29c899b7ca9e506c1cf3ea
honsumal <14leealastair@gmail.com>
2022-11-27 11:50:31 -0500

    edited about

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -8,7 +8,7 @@ export default function About() {
       <div className='container'>
         
         <p>
-          Full Stack Web Developer with a background in Chemical Engineering. Currently pursuing a certificate in Full Stack Development from the University of Toronto, with skills in JavaScript, MySQL, MongoDB, and Python. Known for being able to work flexibly as a team member or team leader in a fast-paced group. Looking to grow as a programmer by building applications that can help in people's everyday lives. Looking to join a company to leverage my current skills, grow my repertoire, and become a valuable asset.
+          Full Stack Web Developer with a background in Chemical Engineering. Currently pursuing a certificate in Full Stack Development from the University of Toronto, with skills in JavaScript, MySQL, MongoDB, and GraphQL. Known for being able to work flexibly as both a team member and team leader in a fast-paced group. Looking to join a company to leverage my current skills, grow my repertoire, and become a valuable asset. Primary language is English, with fluency in Cantonese and a background in Mandarin.
         </p>
         
         <img src={require('../../images/Self.jpg')} alt = "self" className = "self"></img>
